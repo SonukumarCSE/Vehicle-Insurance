bf6df7d2bccec8707efd9b66fd318ae776c747c8
Sonukum <Sonu_Kumar@gap.com>
2025-05-01 22:11:45 +0530

    enh:folder structure created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sonu Kumar",
+    author_email="sonu1372003@gmail.com",
+    packages=find_packages()
+)
